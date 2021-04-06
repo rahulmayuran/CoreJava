7bfd92d3ff0316cf02c972fff14851d12348d169
rahulks <rahulmayuran1996@gmail.com>
2021-04-06 18:55:17 +0530

    Made changes to properly do binary search with the logarithmic time 0(logn)

--- src/com/DSalgo/Algorithm/BinarySearch.java
+++ src/com/DSalgo/Algorithm/BinarySearch.java
@@ -8,32 +8,39 @@ by breaking the array into two parts with a mid element,
 2. If target element appears on left, then decrement from mid element
 3. If target element appears on right, then increment from mid element
 */
-public class BinarySearch {
+public class BinarySearch { //Time complexity is O(logn)
 
     public static void main(String[] args) {
-        binarySearch();
-    }
 
-    public static int binarySearch() {
-        int arrayToSearch[] = {12, 22, 10, 9, 18, 19, 20};
-        int target = 2;
+        int[] arrayToSearch = {2,4,6,8,10,12,14}; //sorted array
+        int target = 14; // Element to search
 
         int low = 0; //left
         int high = arrayToSearch.length - 1; //right
-        int mid = (low + high) / 2;
+
 
         while (low <= high) {
+            int mid = (low + high)/2;
+            System.out.println("Mid Value is ("+low+" + "+high+")/2 = "+mid);
             if (arrayToSearch[mid] == target) { //pos{3} is mid
                 System.out.println("The target element -> " + arrayToSearch[mid] + " is found at position " + mid);
-                return mid;
-            } else if (arrayToSearch[mid] < target) {
-                mid = mid - 1;
-            } else if (arrayToSearch[mid] > target) {
-                mid = mid + 1;
+
+                if(arrayToSearch[mid] != target) {
+                    System.out.println("Element not found in array tray");
+                }
+                break;
+            }
+            else if (arrayToSearch[mid] < target) { //If target is greater than 8, traversing right side
+                low = mid + 1;
+                System.out.println("Low value -> "+low+" is incremented from mid "+mid);
+            } else if (arrayToSearch[mid] > target) {  //If target is lesser than 8, traversing left side
+                high = mid - 1;
+                System.out.println("High value -> "+high+" is decremented from mid "+mid);
             }
 
         }
-        return mid;
+
+
     }
 
 }
