41c43e94788e48bd9da0280f1474d408be42d9bd
rahulmayuran <rahulmayuran1996@gmail.com>
2022-08-14 23:59:39 +0530

    Added iterative and recursive approach

--- src/com/DSalgo/Algorithm/BinarySearch.java
+++ src/com/DSalgo/Algorithm/BinarySearch.java
@@ -1,44 +1,57 @@
 package com.DSalgo.Algorithm;
 
-import static com.BaseLogger.Logger.commmonLogger;
+/** Efficient way of searching the target element in the array of elements
+ * by breaking the array into two parts with a mid element.
+ *
+ * Array must be sorted to do this.
+ * If mid element is the target element, return it
+ *
 
-/*
-Efficient way of searching the target element in the array of elements
-by breaking the array into two parts with a mid element,
-
-1. If mid element is the target element, return it
-2. If target element appears on left, then decrement from mid element
-3. If target element appears on right, then increment from mid element
-
-Visualize it!!
 */
 public class BinarySearch {
 
     public static void main(String[] args) {
-        binarySearch();
-    }
 
-    public static int binarySearch() {
         int[] arrayToSearch = {12, 22, 10, 9, 18, 19, 20};
-        int target = 19;
-
-        int low = 0; //left
-        int high = arrayToSearch.length - 1; //right
-        int mid = (low + high) / 2;
-
-        while (low <= high) {
-            if (arrayToSearch[mid] == target) { //pos{3} is mid
-                commmonLogger.info("The target element -> " + arrayToSearch[mid]
-                        + " is found at position " + mid);
-                return mid;
-            } else if (arrayToSearch[mid] < target) {
-                mid = mid - 1;
-            } else if (arrayToSearch[mid] > target) {
-                mid = mid + 1;
-            }
-
-        }
-        return mid;
+        System.out.println("Iterative ->" + iterativeBinarySearch(arrayToSearch, 18));
+        System.out.println("Recursive -> " + recursiveBinarySearch(arrayToSearch, 18));
     }
 
+    public static String iterativeBinarySearch(int[] intArray, int value) {
+
+        int start = 0;
+        int end = intArray.length;
+
+        while(start < end){
+            int mid = (start + end) / 2;
+            if(intArray[mid] == value) {
+                return "Value is located at index " + mid;
+                // Right half
+            } else if (intArray[mid] < value) {
+                 start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return "The provided value -> " + value + " is not found";
+    }
+
+    public static String recursiveBinarySearch(int[] intArray, int value){
+        return recursiveSearch(intArray, 0, intArray.length, value);
+    }
+
+    private static String recursiveSearch(int[] intArray, int start,  int end,  int value) {
+
+            if(start >= end){
+                return "Value not found";
+            }
+            int mid = (start + end) / 2;
+            if(intArray[mid] == value){
+                return "Value is found at index -> " + mid ;
+            } else if (intArray[mid] < value) {
+                return recursiveSearch(intArray, mid + 1, end, value);
+            } else {
+                return recursiveSearch(intArray, start, mid, value);
+            }
+    }
 }
